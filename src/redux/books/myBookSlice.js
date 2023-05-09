c94e9d9796f286594d301ff9378c9b32e707c7fa
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-05-09 13:55:27 +0500

    Add categoryslice and mybookslice

---
+++ src/redux/books/myBookSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {myBooks:[]};
+const myBooksSlice = createSlice({
+    name: books,
+    initialState,
+    reducers: {
+        addBooks: (state,action) =>{
+            state.myBooks.push(action.payload)
+        },
+         
+    },
+    removedBooks: (state,action) =>{
+        state.books = state.books.filter((book)=> book.id!==action.payloaded)
+    },
+    
+})
+
+export const { addBooks, removedBooks } = myBooksSlice.actions;
+export default myBooksSlice.reducer;
