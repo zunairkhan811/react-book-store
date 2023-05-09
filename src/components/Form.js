25795bbd330f0737f0d0079863db72caada32c02
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-05-09 18:36:33 +0500

    install uuid

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,14 @@
+
 const MyForm = () => (
+
   <>
+    <form onSubmit={handleclick}>
     <input type="text" id="title" placeholder="Your Title Here..." />
     <br />
     <input type="text" id="name" placeholder="Your Name Here..." />
     <br />
     <button type="submit">Add Book</button>
+    </form>
   </>
 );
 
