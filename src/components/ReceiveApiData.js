d1c933076d46efb0a5344df13cd6175be2b8753f
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-05-10 17:07:25 +0500

    install axios

---
+++ src/components/ReceiveApiData.js
@@ -0,0 +1,6 @@
+import axios from "axios"
+
+const fetchData = async () =>{
+  const data = await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/8uAGkqGHBMzF9G5Da8NZ/books',
+  )
+}
