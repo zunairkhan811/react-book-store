3d3b02f7bcb8dd35a8f1081f5880ddc4e646c91c
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-06-03 03:57:39 +0500

    delete extra file

--- src/redux/rootReducers.js
+++ src/redux/rootReducers.js
@@ -1,10 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import booklists from './books/myBookSlice';
-import myCategorySlice from './categories/categoriesSlice';
 
 const rootReducer = combineReducers({
   books: booklists,
-  categories: myCategorySlice,
 });
 
 export default rootReducer;
